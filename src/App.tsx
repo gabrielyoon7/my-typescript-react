e2fd6f3c1fb9cbaaceeb429d2ca65a29bb11a4bb
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-18 21:19:03 +0900

    feat: Typescript로 카운터 예제 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const increment = (): void => {
+    setCount(count + 1);
+  };
+
+  const decrement = (): void => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>증가</button>
+      <button onClick={decrement}>감소</button>
     </div>
   );
-}
+};
 
 export default App;
