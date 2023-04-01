58b60523f7cabfdabfbc21e1236dcc9d4c9c5708
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-04-01 10:27:41 +0900

    refactor: 파일 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css';
-import Counter1 from './Counter1';
-import Counter2 from './Counter2';
+import Counter1 from './containers/Counter1';
+import Counter2 from './containers/Counter2';
 
 function App() {
   return (
     <div>
-      <Counter1/>
-      <hr/>
-      <Counter2/>
-      <hr/>
+      <Counter1 />
+      <hr />
+      <Counter2 />
+      <hr />
     </div>
   );
 }
