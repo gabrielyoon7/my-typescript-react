e63d970e70cb0456b1168f58156906e72f88fc47
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-04-01 10:35:57 +0900

    feat: 배열 관리기 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
-import Counter1 from './containers/Counter1';
-import Counter2 from './containers/Counter2';
+import ItemArray from './containers/array/ItemArray';
+import Counter1 from './containers/counter/Counter1';
+import Counter2 from './containers/counter/Counter2';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <hr />
       <Counter2 />
       <hr />
+      <ItemArray />
     </div>
   );
 }
---
+++ src/containers/array/ItemArray.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+function ItemArray() {
+  const [list, setList] = useState<string[]>([]);
+
+  const addItem = () => {
+    setList(list.concat(`Item ${list.length}`));
+  }
+  const removeItem = () => {
+    setList(list.filter((el) => el !== list[list.length - 1]));
+  }
+
+  return (
+    <>
+      <ul>
+        {list.map((el) => <li key={el}>{el}</li>)}
+      </ul>
+      <button onClick={() => addItem()}>추가</button>
+      <button onClick={() => removeItem()}>삭제</button>
+    </>
+  )
+}
+export default ItemArray
