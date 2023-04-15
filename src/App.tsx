26c7371c73ac259817719aaee29ab2622679cf9c
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-04-16 02:03:39 +0900

    refactor: 불필요한 파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import './App.css';
 import ArrayRoot from './containers/array/ArrayRoot';
-import ItemArray from './containers/array/ItemArray';
-import ItemObjectArray from './containers/array/ItemObjectArray';
-import Child from './containers/children/Child';
-import Wrapper from './containers/children/Wrapper';
 import WrapperRoot from './containers/children/WrapperRoot';
-import Counter1 from './containers/counter/Counter1';
-import Counter2 from './containers/counter/Counter2';
 import CounterRoot from './containers/counter/CounterRoot';
 
 function App() {
