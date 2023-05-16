16febffd4bf0ec8d552066aef28b0f540c6a2466
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-16 23:29:53 +0900

    feat: 메모이제이션 된 박스를 여러개 둬보기

--- src/pages/optimization/NonOptimization.tsx
+++ src/pages/optimization/NonOptimization.tsx
@@ -1,9 +1,9 @@
 import { memo, useState } from "react";
 
-function Box() {
-  console.log('Box 렌더링 됨')
+function Box({ color }: { color: string }) {
+  console.log(`Box 렌더링 됨 : ${color}`)
   return (
-    <div style={{ width: "100px", height: "100px", margin: '3px', backgroundColor: 'red' }} />
+    <div style={{ width: "100px", height: "100px", margin: '3px', backgroundColor: color }} />
   )
 }
 
@@ -11,16 +11,23 @@ const MemoedBox = memo(Box);
 
 function NonOptimization() {
   const [appRenderCount, setAppRenderCount] = useState(0);
+  const [color, setColor] = useState('red');
 
   console.log(`랜더링 횟수 : ${appRenderCount}`);
 
   return (
     <>
-      <MemoedBox />
+      <MemoedBox color={color} />
+      <MemoedBox color={color === 'red' ? 'blue' : 'red'} />
       <button
         onClick={() => setAppRenderCount(appRenderCount + 1)}
       >
         앱 다시 렌더링 하기
+      </button>
+      <button
+        onClick={() => setColor(color === 'red' ? 'blue' : 'red')}
+      >
+        색상 바꾸기
       </button>
     </>
   )
