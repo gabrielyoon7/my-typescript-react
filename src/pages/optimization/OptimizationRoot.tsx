324418dab945899c4d2478af726768444ed53dc7
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-17 00:55:59 +0900

    refactor: 폴더 구조 변경

--- src/pages/optimization/OptimizationRoot.tsx
+++ src/pages/optimization/OptimizationRoot.tsx
@@ -1,14 +1,17 @@
-import NonOptimization from "./NonOptimization";
-import Optimization from "./Optimization";
+import NonOptimization from "./useMemo/NonOptimization";
+import Optimization from "./useMemo/Optimization";
 
 function OptimizationRoot() {
   return (
     <>
-      <h1>최적화</h1>
+      <h1>최적화 by useMemo()</h1>
       <h2>최적화가 안된 예제</h2>
       <NonOptimization />
       <h2>최적화가 된 예제</h2>
       <Optimization />
+      <h1>최적화 by useCallback()</h1>
+      <h2>최적화가 안된 예제</h2>
+      <h2>최적화가 된 예제</h2>
     </>
   )
 }
