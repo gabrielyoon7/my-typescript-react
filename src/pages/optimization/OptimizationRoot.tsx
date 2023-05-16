9285f68cb002e46ceb7730b2202476dc79e6dd5b
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-17 01:05:58 +0900

    refactor: 폴더 구조 개선
    - useCallback 예제 추가를 위한 구조 개선

--- src/pages/optimization/OptimizationRoot.tsx
+++ src/pages/optimization/OptimizationRoot.tsx
@@ -1,14 +1,14 @@
-import NonOptimization from "./useMemo/NonOptimization";
-import Optimization from "./useMemo/Optimization";
+import NonOptimizationByMemo from "./useMemo/NonOptimizationOptimizationByMemo";
+import OptimizationByMemo from "./useMemo/OptimizationByMemo";
 
 function OptimizationRoot() {
   return (
     <>
       <h1>최적화 by useMemo()</h1>
       <h2>최적화가 안된 예제</h2>
-      <NonOptimization />
+      <NonOptimizationByMemo />
       <h2>최적화가 된 예제</h2>
-      <Optimization />
+      <OptimizationByMemo />
       <h1>최적화 by useCallback()</h1>
       <h2>최적화가 안된 예제</h2>
       <h2>최적화가 된 예제</h2>
---
+++ src/pages/optimization/useMemo/OptimizationByMemo.tsx
@@ -0,0 +1,39 @@
+import { memo, useMemo, useState } from "react";
+
+function Box({ params }: {
+  params: { color: string }
+}) {
+  console.log(`Box 렌더링 됨 : ${params.color}`)
+  return (
+    <div style={{ width: "100px", height: "100px", margin: '3px', backgroundColor: params.color }} />
+  )
+}
+
+const MemoedBox = memo(Box);
+
+function OptimizationByMemo() {
+  const [appRenderCount, setAppRenderCount] = useState(0);
+  const [color, setColor] = useState('red');
+
+  console.log(`랜더링 횟수 : ${appRenderCount}`);
+
+  const params = useMemo(() => ({ color }), [color]);
+
+  return (
+    <>
+      <MemoedBox params={params} />
+      <button
+        onClick={() => setAppRenderCount(appRenderCount + 1)}
+      >
+        앱 다시 렌더링 하기
+      </button>
+      <button
+        onClick={() => setColor(color === 'red' ? 'blue' : 'red')}
+      >
+        색상 바꾸기
+      </button>
+    </>
+  )
+}
+
+export default OptimizationByMemo;
