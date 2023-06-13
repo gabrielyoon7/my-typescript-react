6e0c6c396f0061c9060c5bb0315018336f049846
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-13 11:30:13 +0900

    refactor: 동적 테이블 타입스크립트 변환

--- src/pages/dynamcTable/Table/TableFooter/index.tsx
+++ src/pages/dynamcTable/Table/TableFooter/index.tsx
@@ -1,9 +1,16 @@
-// @/src/components/Table/TableFooter/index.jsx
-import React, { useEffect } from "react";
+// @/src/components/Table/TableFooter/index.tsx
+import React, {useEffect} from "react";
 
 import styles from "./TableFooter.module.css";
 
-const TableFooter = ({ range, setPage, page, slice }) => {
+interface TableFooterProps {
+  range: number[];
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+  page: number;
+  slice: any[];
+}
+
+const TableFooter = ({range, setPage, page, slice}: TableFooterProps) => {
   useEffect(() => {
     if (slice.length < 1 && page !== 1) {
       setPage(page - 1);
@@ -12,8 +19,8 @@ const TableFooter = ({ range, setPage, page, slice }) => {
 
   let temp = range;
   if (range.length > 6) {
-    let start = parseInt(page) - 3;
-    let end = parseInt(page) + 3;
+    let start = page - 3;
+    let end = page + 3;
     if (start <= 0) {
       start = 0;
     }
@@ -29,7 +36,7 @@ const TableFooter = ({ range, setPage, page, slice }) => {
         <button
           key={index}
           className={`${styles.button} ${page === el ? styles.activeButton : styles.inactiveButton
-            }`}
+          }`}
           onClick={() => setPage(el)}
         >
           {el}
---
+++ src/pages/dynamcTable/Table/index.tsx
@@ -0,0 +1,42 @@
+// @/src/components/Table/index.tsx
+import React, {useState} from "react";
+import useTable from "./hooks/useTable";
+import styles from "./Table.module.css";
+import TableFooter from "./TableFooter";
+
+interface TableProps {
+  data: any; // 수정이 필요한 부분
+  rowsPerPage: number;
+}
+
+const Table: React.FC<TableProps> = ({data, rowsPerPage}) => {
+  const [page, setPage] = useState<number>(1);
+  const {slice, range} = useTable(data, page, rowsPerPage);
+
+  return (
+    <>
+      <table className={styles.table}>
+        <thead className={styles.tableRowHeader}>
+        <tr>
+          {Object.keys(data[0]).map((el) => (<th key={Math.random()} className={styles.tableHeader}>{el}</th>))}
+        </tr>
+        </thead>
+        <tbody>
+        {slice.map((el) => (
+          <tr className={styles.tableRowItems} key={Math.random()}>
+            {Object.keys(el).map((col) => (<td key={Math.random()} className={styles.tableCell}>{el[col]}</td>))}
+          </tr>
+        ))}
+        </tbody>
+      </table>
+      <TableFooter
+        range={range}
+        slice={slice}
+        page={page}
+        setPage={setPage}
+      />
+    </>
+  );
+};
+
+export default Table;
