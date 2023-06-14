5bfef0d387ce15a3c202f07a83f7a7e8bff12a7c
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-14 22:36:19 +0900

    chore: msw 설치

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import router from "./router.tsx";
 import {RouterProvider} from "react-router-dom";
+import {worker} from "./mocks/browser.ts";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-      <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+async function main() {
+
+  await worker.start({
+    serviceWorker: {
+      url: '/my-typescript-react/mockServiceWorker.js',
+    },
+  })
+
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <RouterProvider router={router}/>
+    </React.StrictMode>,
+  )
+}
+
+main();
