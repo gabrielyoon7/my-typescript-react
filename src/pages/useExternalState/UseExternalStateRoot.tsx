174e9bea43be3e44fb2ce9a0ee72f90cd7b57d59
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-20 11:42:39 +0900

    refactor: 불필요한 코드 제거

--- src/pages/useExternalState/UseExternalStateRoot.tsx
+++ src/pages/useExternalState/UseExternalStateRoot.tsx
@@ -3,7 +3,6 @@ import TodoContainer from "./components/TodoContainer.tsx";
 function UseExternalStateRoot() {
   return (
     <>
-      <h1>미완성</h1>
       <TodoContainer/>
       <hr/>
       <TodoContainer/>
