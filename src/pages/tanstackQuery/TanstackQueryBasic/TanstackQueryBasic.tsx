3bf7480e38ae17697ce4094c719812edc0b0d117
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-23 01:44:51 +0900

    fix: devtools 중복 버그 수정

--- src/pages/tanstackQuery/TanstackQueryBasic/TanstackQueryBasic.tsx
+++ src/pages/tanstackQuery/TanstackQueryBasic/TanstackQueryBasic.tsx
@@ -2,37 +2,31 @@ import axios, {AxiosError} from 'axios';
 import {
   useQuery,
   useQueryClient,
-  QueryClient,
-  QueryClientProvider,
 } from '@tanstack/react-query';
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import {Dispatch, SetStateAction, useState} from "react";
 import {Todo} from "./type.ts";
 
-const queryClient = new QueryClient();
 
 function TanstackQueryBasic() {
   const [postId, setPostId] = useState<number>(-1);
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <p>
-        As you visit the posts below, you will notice them in a loading state
-        the first time you load them. However, after you return to this list and
-        click on any posts you have already visited again, you will see them
-        load instantly and background refresh right before your eyes!{' '}
+        <div>
+          아래 게시물을 방문하면 처음 게시물을 로드할 때 로드 상태로 표시됩니다. 그러나 이 목록으로 돌아가서 이미 방문한 게시물을 다시 클릭하면 바로 로드되고 배경이 새로 고쳐지는 것을 볼 수 있습니다!
+        </div>
         <strong>
-          (You may need to throttle your network speed to simulate longer
-          loading sequences)
+          (더 긴 로드 시퀀스를 시뮬레이션하기 위해 네트워크 속도를 조절해야 할 수도 있습니다)
         </strong>
       </p>
-      {postId > -1 ? (
-        <Post postId={postId} setPostId={setPostId}/>
-      ) : (
-        <Posts setPostId={setPostId}/>
-      )}
-      <ReactQueryDevtools initialIsOpen/>
-    </QueryClientProvider>
+      {
+        postId > -1 ? (
+          <Post postId={postId} setPostId={setPostId}/>
+        ) : (
+          <Posts setPostId={setPostId}/>
+        )}
+    </>
   );
 }
 
