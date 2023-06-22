af5a30d27b859ad969e3b101514cabe56a084155
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-22 14:22:09 +0900

    chore: recoil 기본 설정

--- src/components/layout/RecoilLayout.tsx
+++ src/components/layout/RecoilLayout.tsx
@@ -1,13 +1,18 @@
 import Layout from "./styles/Layout.tsx";
 import {Outlet} from "react-router-dom";
 import {recoilChildren} from "../../router/routes/children/recoilChildren.tsx";
+import {RecoilRoot} from "recoil";
 
 export default function RecoilLayout() {
   return (
     <Layout
       title="Recoil with Todo"
       routes={recoilChildren}
-      render={<Outlet/>}
+      render={
+        <RecoilRoot>
+          <Outlet/>
+        </RecoilRoot>
+      }
     />
   );
 
