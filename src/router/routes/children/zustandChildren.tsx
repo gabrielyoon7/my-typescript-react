db26a3f2aeb5717b8375303aad086a42e17793c3
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-24 14:40:13 +0900

    feat: zustand 카운터 예제 추가

--- src/router/routes/children/zustandChildren.tsx
+++ src/router/routes/children/zustandChildren.tsx
@@ -1,9 +1,15 @@
 import {Route} from "../../../types/common.ts";
+import Counter from "../../../pages/zustand/Counter";
 
 export const zustandChildren: Route[] = [
   {
     path: '',
     element: <>zustand를 테스트하는 페이지입니다.</>,
     title: 'README'
+  },
+  {
+    path: 'counter',
+    element: <Counter/>,
+    title: 'counter'
   },
 ];
