ed9b9f4d8072dcf6db38fa482f8f8e6eac0ef731
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-25 12:22:41 +0900

    refactor: zustand counter hook 분리

--- src/pages/zustand/counter/Counter.tsx
+++ src/pages/zustand/counter/Counter.tsx
@@ -1,14 +1,4 @@
-import {create} from 'zustand';
-
-type Store = {
-  count: number
-  inc: () => void
-}
-
-const useStore = create<Store>()((set) => ({
-  count: 1,
-  inc: () => set((state) => ({count: state.count + 1})),
-}));
+import {useStore} from "./useStore.tsx";
 
 function Counter() {
   const {count, inc} = useStore();
