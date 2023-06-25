ee48bc6b51cbdec65277e3bf0c9bc2ba0674870a
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-25 12:36:52 +0900

    refactor: zustand getState 예제 구현

--- src/pages/zustand/todo/TodoRoot.tsx
+++ src/pages/zustand/todo/TodoRoot.tsx
@@ -1,11 +1,17 @@
 import TodoExample from "./TodoExample.tsx";
+import Counter from "../counter/Counter.tsx";
 
 function TodoRoot() {
   return (
     <>
+      <h3>Counter Example</h3>
+      <Counter/>
+      <hr/>
+      <hr/>
       <TodoExample/>
       <hr/>
       <TodoExample/>
+      <hr/>
     </>
   );
 }
--- src/pages/zustand/todo/useTodo.ts
+++ src/pages/zustand/todo/useTodo.ts
@@ -1,20 +1,24 @@
 import {create} from 'zustand';
 import {Todo} from "./type.ts";
+import {useCounter} from "../counter/useCounter.tsx";
 
 type Store = {
   todos: Todo[];
   addTodo: (newId: number) => void;
 }
 
-export const useTodo = create<Store>()((set) => ({
+export const useTodo = create<Store>((set) => ({
   todos: [],
-  addTodo: (newId) => set((state) => ({
-    todos: [
-      ...state.todos, {
-        id: newId,
-        content: `#todo : ${newId}`,
-        checked: false
-      }
-    ]
-  })),
+  addTodo: (newId) => {
+    useCounter.getState().inc();
+    set((state) => ({
+      todos: [
+        ...state.todos, {
+          id: newId,
+          content: `#todo : ${newId}`,
+          checked: false
+        }
+      ]
+    }));
+  },
 }));
