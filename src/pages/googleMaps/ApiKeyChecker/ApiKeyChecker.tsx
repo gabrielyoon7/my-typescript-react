26128b49ac4e4f7830c5549af21237a537678c9a
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-13 00:21:16 +0900

    feat: api key를 로컬 스토리지에 저장할 수 있는 기능 구현

--- src/pages/googleMaps/ApiKeyChecker/ApiKeyChecker.tsx
+++ src/pages/googleMaps/ApiKeyChecker/ApiKeyChecker.tsx
@@ -1,33 +1,71 @@
-import {ReactNode, useRef, useState} from "react";
+import { ReactNode, useEffect, useRef, useState } from 'react';
+import { getLocalStorage, setLocalStorage } from '../../../utils/storage';
+import {
+  LOCAL_KEY_GOOGLE_MAPS_API,
+  LOCAL_KEY_GOOGLE_MAPS_API_SAVE,
+} from '../../../store/storageKeys';
 
 interface ApiKeyCheckerProps {
-  render: (apiKey: string) => ReactNode
+  render: (apiKey: string) => ReactNode;
 }
 
-function ApiKeyChecker({render}: ApiKeyCheckerProps) {
+function ApiKeyChecker({ render }: ApiKeyCheckerProps) {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [apiKey, setApiKey] = useState("");
+  const [apiKey, setApiKey] = useState('');
+  const [isSaveMode, setSaveMode] = useState(true);
+  useEffect(() => {
+    const isSaving = getLocalStorage<boolean>(
+      LOCAL_KEY_GOOGLE_MAPS_API_SAVE,
+      true
+    );
+    setSaveMode(isSaving);
+    if (isSaving) {
+      const backUpKey = getLocalStorage<string>(LOCAL_KEY_GOOGLE_MAPS_API, '');
+      (inputRef.current as HTMLInputElement).value = backUpKey;
+    }
+  }, []);
+
   const onClick = () => {
     const inputValue = inputRef.current?.value;
     if (inputValue && inputValue.length > 0) {
+      if (isSaveMode) {
+        setLocalStorage(LOCAL_KEY_GOOGLE_MAPS_API, inputValue);
+      } else {
+        setLocalStorage(LOCAL_KEY_GOOGLE_MAPS_API, '');
+      }
       setApiKey(inputValue);
     } else {
-      alert("제대로 된 키를 입력하셔야 합니다.");
+      alert('제대로 된 키를 입력하셔야 합니다.');
     }
   };
+
   return (
     <div>
-      {
-        apiKey.length > 0 ? render(apiKey) : (
+      {apiKey.length > 0 ? (
+        render(apiKey)
+      ) : (
+        <div>
+          <div>API키를 입력하세요</div>
+          <input type="text" ref={inputRef} />
+          <button type="submit" onClick={onClick}>
+            전송
+          </button>
           <div>
-            <div>API키를 입력하세요</div>
-            <input type="text" ref={inputRef}/>
-            <button type="submit" onClick={onClick}>
-              전송
-            </button>
+            <input
+              type="checkbox"
+              checked={isSaveMode}
+              onChange={(e) => {
+                setLocalStorage(
+                  LOCAL_KEY_GOOGLE_MAPS_API_SAVE,
+                  e.target.checked
+                );
+                setSaveMode(e.target.checked);
+              }}
+            />
+            키를 브라우저에 저장하기
           </div>
-        )
-      }
+        </div>
+      )}
     </div>
   );
 }
