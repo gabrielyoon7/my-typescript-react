8f1c7be02d7f9c1b43a89ec6e5a3b1084a41712f
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-18 16:11:01 +0900

    chore: npm 배포 수정

--- src/pages/react/Temp.tsx
+++ src/pages/react/Temp.tsx
@@ -1,41 +1,17 @@
-/**
- * 컴포넌트 하나만 테스트 할 때 사용
- */
-
-import { memo, useCallback, useMemo, useState } from "react";
-
-function Box({ params, onClick }: {
-  params: { color: string },
-  onClick: () => void
-}) {
-  console.log('Box 렌더링 됨');
-  return (
-    <div
-      style={{ width: "100px", height: "100px", margin: '3px', backgroundColor: params.color }}
-      onClick={onClick}
-    />
-  );
-}
-const MemoedBox = memo(Box);
+import {Container, Grid} from "@gabrielyoon7/layout-component";
 
 function Temp() {
-  const [appRenderCount, setAppRenderCount] = useState(0);
-  const [color, setColor] = useState('red');
-
-  const params = useMemo(() => ({ color }), [color]);
-  const onClick = useCallback(() => { }, []);
-  console.log(`렌더링 횟수: ${appRenderCount}`);
-
   return (
-    <>
-      <MemoedBox params={params} onClick={onClick} />
-      <button onClick={() => setAppRenderCount(appRenderCount + 1)}>
-        앱 다시 렌더링 하기
-      </button>
-      <button onClick={() => setColor(color === 'red' ? 'blue' : 'red')}>
-        색상 바꾸기
-      </button>
-    </>
+    <Container>
+      <Grid rows={3} columns={3}>
+        <div>하이</div>
+        <div>하이</div>
+        <div>하이</div>
+        <div>하이</div>
+        <div>하이</div>
+        <div>하이</div>
+      </Grid>
+    </Container>
   );
 }
 
